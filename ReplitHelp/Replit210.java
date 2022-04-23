83ac6ce155eb60d577bab484d3835e7f78bc76f0
tarik-abraham <tarik.abraham97@gmail.com>
2022-04-23 00:24:08 -0400

    Replits

---
+++ ReplitHelp/Replit210.java
@@ -0,0 +1,28 @@
+package ReplitHelp;
+
+public class Replit210 {
+
+/**
+ * Complete the code, handle possible exception
+ *
+ * Expected Output:
+ *
+ * / by zero
+ *
+ */
+public static void main(String[] args) {
+
+    int a=10, b=0,result;
+
+   try{
+       result = a/b;
+   }catch(ArithmeticException e){
+       System.out.println(e.getMessage());
+   }
+
+
+
+
+}
+
+}
---
+++ ReplitHelp/Replit211.java
@@ -0,0 +1,17 @@
+package ReplitHelp;
+public class Replit211 {
+    public static void main (String[] args) {
+        try {
+            int[] a = {1};
+            System.out.println(a[4]);
+        } catch (ArrayIndexOutOfBoundsException e){
+            e.printStackTrace();
+
+        }
+
+    }
+}
+
+
+
+
